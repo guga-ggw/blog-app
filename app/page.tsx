b10a78ed778500272709dcd005ee1f5a48394a27
guga-ggw <ggwomega15@gmail.com>
2024-02-15 22:10:53 +0400

    hmm

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,94 @@
+'use client'
 import Image from "next/image";
+import './styles/index.scss'
+import Nav from "@/components/Nav";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
+  const [regform, setregform] = useState(false)
+  const [regtype, setregtype] = useState('')
+
+  const regModul =  (type : string) => {
+    type == "signup" ? setregtype('signup') : setregtype("signin")
+    setregform(true)
+  }
+
+
+  
+  type TPrivateDetailsSchema = z.infer<typeof PrivateDetailsSchema>
+  type TKnownAsSchema = z.infer<typeof KnownAsSchema>
+
+  const PrivateDetailsSchema = z.object({
+    email: z.string().email('enter valid email'),
+    password: z.string().min(8, 'passwords must be at least 8 characters'),
+    confirmPassword: z.string().min(8, 'confirm passwords must be at least 8 characters')
+  }).refine((data) => data.confirmPassword == data.password, {
+    message : "Passwords doesn't match",
+    path : ['confirmPassword']
+  })
+
+  const KnownAsSchema = z.object({
+    name: z.string().min(2, 'Name must be at least 2 characters'),
+    NickName: z.string().min(5, 'NickName must be at least 5 characters'),
+  })
+
+  const validationSchema = nextstep ? PrivateDetailsSchema : KnownAsSchema;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+    setError
+  } = useForm<TPrivateDetailsSchema>({
+    resolver : zodResolver(validationSchema),
+  })
+
+  const Fsubmit = (data: TPrivateDetailsSchema | TKnownAsSchema) => {
+    dispatch(setUserInfo(data))
+    dispatch(nextStep(true))
+    reset()
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="w-full min-h-[100vh] bg-[#181818] flex flex-col items-center justify-center">
+      {/* <Nav/> */}
+      {regform && (
+        <>
+        <div className="w-full h-full bg-black bg-opacity-55 top-0 left-0 absolute"></div>
+        <div className="flex flex-col items-center justify-center absolute left-1/2 py-1 md:py-6 top-1/2 translate-x-[-50%] translate-y-[-50%] w-5/6 h-2/3 rounded-lg bg-[#8CADA7] md:w-[450px]">
+          <p onClick={() => {
+            setregform(false)
+            setregtype('')
+          }} className="absolute right-4 top-2 cursor-pointer">X</p>
+          <h1 className="text-3xl text-white">{regtype == "signup" ? "Sign up" : "Sign in"}</h1>
+          {regtype == "signup" ? (
+            <>
+          <div className="flex flex-col h-[50%] justify-between w-full px-10 md:gap-12 mt-3 md:mt-12">
+          <input className="h-10 md:h-12 rounded-lg outline-none px-2 text-black shadow-lg shadow-black" placeholder="Enter NickName" type="text" />
+          <input className="h-10 md:h-12 rounded-lg outline-none px-2 text-black shadow-lg shadow-black" placeholder="Enter email" type="text" />
+          <input className="h-10 md:h-12 rounded-lg outline-none px-2 text-black shadow-lg shadow-black" placeholder="Enter password" type="password" />
+          <input className="h-10 md:h-12 rounded-lg outline-none px-2 text-black shadow-lg shadow-black" placeholder="Confirm password" type="password" />
+          </div>
+          <button className="w-fit px-2 md:px-9 h-fit py-3 mt-6 md:py-5 rounded-xl shadow-lg shadow-[#181818] bg-[#181818] text-white">Create Account</button>
+          <p onClick={() => setregtype('signin')} className="cursor-pointer text-[15px] text-blue-900 underline mt-4">already have an account?</p>
+            </>
+          ) : (<>
+           <div className="flex flex-col h-3/5 md:h-2/5 w-full px-10 gap-12 mt-12 md:mt-24 py-7">
+          <input className="h-12 rounded-lg outline-none px-2 text-black shadow-lg shadow-black" placeholder="Enter email" type="text" />
+          <input className="h-12 rounded-lg outline-none px-2 text-black shadow-lg shadow-black" placeholder="Enter password" type="password" />
+          </div>
+          <button className="w-fit px-9 py-3 md:px-9 h-fit md:py-5 mt-2 rounded-xl shadow-lg shadow-[#181818] bg-[#181818] text-white">LogIn</button>
+          <p onClick={() => setregtype('signup')} className="cursor-pointer text-[15px] text-blue-900 underline mt-4">Do not have an account?</p>
+          </>)}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </>
+      )}
+      <h1 id="head_txt" className="text-[#F2F4CB] text-4xl text-center px-3 xl:text-[70px]">Register to Gugatweet for amazing experience</h1>
+      <div className="w-full lg:w-[60%] flex items-center justify-center gap-11 xl:gap-32 mt-8 xl:mt-24">
+        <button onClick={() => regModul('signup')} className="xl:w-48 xl:h-16 xl:text-[25px] w-32 h-12 bg-[#F2F4CB] rounded-xl shadow-2xl shadow-black">Sign up</button>
+        <button onClick={() => regModul('signin')}  className="xl:w-48 xl:h-16 xl:text-[25px] w-32 h-12 bg-[#F2F4CB] rounded-xl shadow-2xl shadow-black">Sign in</button>
       </div>
     </main>
   );
---
+++ components/Nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='w-full h-20 bg-[#110B11] flex items-center text-[#110B11] justify-end px-7'>
+        <h2 className='w-28 text-xl h-11 bg-[#F2F4CB] flex items-center cursor-pointer justify-center rounded-2xl'>Register</h2>
+    </div>
+  )
+}
+
+export default Nav
